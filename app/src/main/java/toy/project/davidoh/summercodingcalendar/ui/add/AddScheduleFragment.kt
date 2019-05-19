8db8935754306cb0f3572fe555bcac6cef651eb6
seungrokoh <devdavidoh@gmail.com>
2019-05-20 00:28:01 +0900

    fix : 일정 등록 화면 버그 수정
    1. 등록 시 Button Enable 설정 부분에서 한 번 체크 한 뒤 버튼 클릭 되지 않는 버그 수정

--- app/src/main/java/toy/project/davidoh/summercodingcalendar/ui/add/AddScheduleFragment.kt
+++ app/src/main/java/toy/project/davidoh/summercodingcalendar/ui/add/AddScheduleFragment.kt
@@ -1,6 +1,5 @@
 package toy.project.davidoh.summercodingcalendar.ui.add
 
-import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
--- app/src/main/java/toy/project/davidoh/summercodingcalendar/ui/add/presenter/AddSchedulePresenter.kt
+++ app/src/main/java/toy/project/davidoh/summercodingcalendar/ui/add/presenter/AddSchedulePresenter.kt
@@ -2,12 +2,9 @@ package toy.project.davidoh.summercodingcalendar.ui.add.presenter
 
 import android.text.TextUtils
 import com.prolificinteractive.materialcalendarview.CalendarDay
-import kotlinx.coroutines.Dispatchers
 import toy.project.davidoh.summercodingcalendar.data.Schedule
 import toy.project.davidoh.summercodingcalendar.data.source.SchedulesDataSource
 import toy.project.davidoh.summercodingcalendar.data.source.SchedulesRepository
-import toy.project.davidoh.summercodingcalendar.util.launchSilent
-import kotlin.coroutines.CoroutineContext
 
 class AddSchedulePresenter(
     private var view: AddScheduleContractor.View,
@@ -28,16 +25,14 @@ class AddSchedulePresenter(
             ), object : SchedulesDataSource.InsertScheduleCallback {
                 override fun onScheduleInserted() {
                     if (view.isActive) {
-                        view.setAddButtonEnable(true)
-                        view.showSuccessMessage("등록 완료")
+                        showMessageWithEnableButton("등록 완료")
                         view.finish()
                     }
                 }
 
                 override fun onInsertFailed() {
                     if (view.isActive) {
-                        view.setAddButtonEnable(true)
-                        view.showErrorMessage("일정 입력에 실패하였습니다.")
+                        showMessageWithEnableButton("일정 입력에 실패하였습니다.")
                     }
                 }
             })
@@ -46,17 +41,22 @@ class AddSchedulePresenter(
 
     private fun isEmpty(date: CalendarDay?, title: String?, description: String?): Boolean {
         if (date == null) {
-            view.showInfoMessage("날짜를 선택해주세요.")
+            showMessageWithEnableButton("날짜를 선택해주세요.")
             return false
         }
         if (TextUtils.isEmpty(title)) {
-            view.showInfoMessage("제목을 입력해주세요.")
+            showMessageWithEnableButton("제목을 입력해주세요.")
             return false
         }
         if (TextUtils.isEmpty(description)) {
-            view.showInfoMessage("내용을 입력해주세요.")
+            showMessageWithEnableButton("내용을 입력해주세요.")
             return false
         }
         return true
+    }
+
+    private fun showMessageWithEnableButton(message: String) {
+        view.showInfoMessage(message)
+        view.setAddButtonEnable(true)
     }
 }
